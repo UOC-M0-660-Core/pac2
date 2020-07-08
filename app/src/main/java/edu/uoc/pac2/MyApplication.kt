a8b87a30f084bf58e03b3639be6cff7a25a21d44
Alex Queudot <alqueraf@gmail.com>
2020-07-08 16:46:58 +0200

    UOC - PAC 2

---
+++ app/src/main/java/edu/uoc/pac2/MyApplication.kt
@@ -0,0 +1,28 @@
+package edu.uoc.pac2
+
+import android.app.Application
+import edu.uoc.pac2.data.*
+
+/**
+ * Entry point for the Application.
+ */
+class MyApplication : Application() {
+
+    private lateinit var booksInteractor: BooksInteractor
+
+    override fun onCreate() {
+        super.onCreate()
+        // TODO: Init Room Database
+        // TODO: Init BooksInteractor
+    }
+
+    fun getBooksInteractor(): BooksInteractor {
+        return booksInteractor
+    }
+
+    fun hasInternetConnection(): Boolean {
+        // TODO: Add Internet Check logic.
+        // Check Min and Target SDK Versions for Android API Compatibilities
+        return true
+    }
+}
